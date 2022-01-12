443e9c28b07d74c75db86202a3d8454ed4a92f13
ghbeta <ghmail2@gmail.com>
2022-01-12 02:20:30 +0100

    suit my needs

--- files/start.sh
+++ files/start.sh
@@ -5,13 +5,7 @@ if [ ! -f /conf/aria2.conf ]; then
 		echo "rpc-secret=${SECRET}" >> /conf/aria2.conf
 	fi
 fi
-if [ ! -f /conf/on-complete.sh ]; then
-	cp /conf-copy/on-complete.sh /conf/on-complete.sh
-fi
 
-chmod +x /conf/on-complete.sh
 touch /conf/aria2.session
 
-darkhttpd /aria2-webui/docs --port 80 &
-darkhttpd /data --port 8080 &
 aria2c --conf-path=/conf/aria2.conf
